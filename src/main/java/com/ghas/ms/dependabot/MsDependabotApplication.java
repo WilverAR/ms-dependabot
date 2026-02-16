2dcfb5e31f45d4eef6c88c5202d7b94ee4b74aad
Wilver Arana Ramos <wilver.arana@softtekprojects.com>
2026-02-16 11:25:05 -0500

    Add GHAS workflows and dependabot

---
+++ src/main/java/com/ghas/ms/dependabot/MsDependabotApplication.java
@@ -0,0 +1,13 @@
+package com.ghas.ms.dependabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDependabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDependabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghas/ms/dependabot/MsDependabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghas.ms.dependabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDependabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
